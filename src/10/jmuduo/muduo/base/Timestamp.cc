bedc716e716547fa8ef91bc42a42a9c6cb3d6234
suirui17 <r13167566588@163.com>
2022-05-24 22:10:33 +0800

    base

--- src/10/jmuduo/muduo/base/Timestamp.cc
+++ src/10/jmuduo/muduo/base/Timestamp.cc
@@ -25,6 +25,7 @@ string Timestamp::toString() const
   int64_t seconds = microSecondsSinceEpoch_ / kMicroSecondsPerSecond;
   int64_t microseconds = microSecondsSinceEpoch_ % kMicroSecondsPerSecond;
   snprintf(buf, sizeof(buf)-1, "%" PRId64 ".%06" PRId64 "", seconds, microseconds);
+  // PRId64
   return buf;
 }
 
@@ -34,25 +35,28 @@ string Timestamp::toFormattedString() const
   time_t seconds = static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond);
   int microseconds = static_cast<int>(microSecondsSinceEpoch_ % kMicroSecondsPerSecond);
   struct tm tm_time;
-  gmtime_r(&seconds, &tm_time);
+  gmtime_r(&seconds, &tm_time);// _r表示线程安全
+  // 该函数可以将秒数转化为tm结构体
+  // tm结构体包含年、月、日、时、分、秒
 
   snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d.%06d",
       tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
       tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec,
       microseconds);
+  // 返回一个格式化的字符串
   return buf;
 }
 
 Timestamp Timestamp::now()
 {
   struct timeval tv;
-  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);// 第二个参数用来返回时区，不需要时区的话，可以设置为空指针
   int64_t seconds = tv.tv_sec;
   return Timestamp(seconds * kMicroSecondsPerSecond + tv.tv_usec);
 }
 
 Timestamp Timestamp::invalid()
 {
-  return Timestamp();
+  return Timestamp(); // 默认构造函数返回的是无效的时间
 }
 
